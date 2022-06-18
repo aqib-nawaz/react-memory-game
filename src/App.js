4f2b0e310c8ddaee025baa7eb7b5dca55f5f9a5b
Aqib Nawaz <aqibnawaz032@gmail.com>
2022-06-18 10:17:11 +0500

    card flipping

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React, { Component } from "react";
 import Button from "./components/Button";
 import Container from "./components/Container";
-import "./App.css"
-import arrayShuffle from 'array-shuffle';
+import "./App.css";
+import arrayShuffle from "array-shuffle";
 
 const imageList = [
-        { name: "bird", src: "./images/bird.png" },
-        { name: "cat", src: "./images/cat.jpg" },
-        { name: "chick", src: "./images/chick.jpg" },
-        { name: "cow", src: "./images/cow.png" },
-        { name: "dog", src: "./images/dog.png" },
-        { name: "duck", src: "./images/duck.jpg" },
-        { name: "fish", src: "./images/fish.jpg" },
-        { name: "panda", src: "./images/panda.jpg" },
-        { name: "rabbit", src: "./images/rabbit.jpg" },
-        { name: "squirrel", src: "./images/squirrel.jpg"}
-]
+  { name: "bird", src: "./images/bird.png" },
+  { name: "cat", src: "./images/cat.jpg" },
+  { name: "chick", src: "./images/chick.jpg" },
+  { name: "cow", src: "./images/cow.png" },
+  { name: "dog", src: "./images/dog.png" },
+  { name: "duck", src: "./images/duck.jpg" },
+  { name: "fish", src: "./images/fish.jpg" },
+  { name: "panda", src: "./images/panda.jpg" },
+  { name: "rabbit", src: "./images/rabbit.jpg" },
+  { name: "squirrel", src: "./images/squirrel.jpg" },
+];
 export default class App extends Component {
   constructor() {
     super();
@@ -24,24 +24,31 @@ export default class App extends Component {
       isFlipped: false,
       firstChoice: "",
       secondChoice: "",
-      cardArray:[]
+      cardArray: [],
     };
   }
 
-gameStartHandler = () => {
-const shuffledList = arrayShuffle(imageList);
+  gameStartHandler = () => {
+    const shuffledList = arrayShuffle(imageList);
 
-  this.setState({
-    cardArray: shuffledList.concat(arrayShuffle(imageList))
-  })
-}
+    this.setState({
+      cardArray: shuffledList.concat(arrayShuffle(imageList)),
+    });
+  };
 
+  choiceHandler = (firstChoice, secondChoice) => {
+
+  };
 
   render() {
-    return <div className="main-container">
-      <Button value="Start" onClick= {this.gameStartHandler}/>
-      <Container imgArray = {this.state.cardArray}/>
-      
-    </div>;
+    return (
+      <div className="main-container">
+        <Button value="Start" onClick={this.gameStartHandler} />
+        <Container
+          imgArray={this.state.cardArray}
+          onClick={this.choiceHandler}
+        />
+      </div>
+    );
   }
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,49 @@
 import React, { Component } from "react";
-import "./Card.css"
+import "./Card.css";
+import { motion } from "framer-motion";
 export default class Card extends Component {
+    constructor() {
+        super();
+        this.state = {
+            isFlipped: false,
+            choice1: "",
+            choice2: "",
+        };
+    }
+
+    cardHandler = (selectCardName) => {
+        console.log(selectCardName);
+        this.setState({
+            isFlipped: !this.state.isFlipped,
+        });
+    };
     render() {
-        let imgClassName = "img " + this.props.className;
         return (
-            <div className="card" onClick={this.props.onClick}>
-                <img
-                    src={require(`../images/${this.props.name}.jpg`)}
-                    alt={this.props.name}
-                    className={imgClassName}
-                />
-            </div>
+            <motion.div
+                className="card"
+                onClick={() => this.cardHandler(this.props.name)}
+            >
+                <motion.div
+                    className="content"
+                    animate={this.state.isFlipped ? { rotateY: 180 } : { rotateY: 0 }}
+                    transition={{ duration: 0.1 }}
+                >
+                    <motion.div className="front">
+                        <img
+                            src={require(`../images/backside.jpg`)}
+                            alt={this.props.name}
+                            className="img"
+                        />
+                    </motion.div>
+                    <motion.div animate={{ rotateY: 180 }} className="back">
+                        <img
+                            className="img"
+                            src={require(`../images/${this.props.name}.jpg`)}
+                            alt={this.props.name}
+                        />
+                    </motion.div>
+                </motion.div>
+            </motion.div>
         );
     }
 }
